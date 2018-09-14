62b816d53852c9869fcbc41959e62af9819e793f
Jani Kurki-Suonio <jani.kurki-suonio@edupoli.fi>
2018-09-14 09:41:59 +0300

    Add project files.

---
+++ TDD_With_SimpleDependencyInjection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_With_SimpleDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
